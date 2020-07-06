15eda6e1e68302db182a1fc05f0cb25a696ddb62
Sam Keener <samuelbkeener@gmail.com>
2020-07-06 14:38:09 -0400

    fixed remove method

--- Classwork/04/SuperArray.java
+++ Classwork/04/SuperArray.java
@@ -24,22 +24,30 @@ public class SuperArray {
 	}
 
 	public void add(int index, int value) {
+		// see if index is out of bounds
 		if (index >= data.length)
 			return;
+		// see if the array is full
 		if (data[data.length - 1] != 0)
 			grow();
+		// move everything after index forward
 		for (int i = data.length - 1; i > index; i--) {
 			data[i] = data[i - 1];
 		}
+		// add value
 		data[index] = value;
 		numberElements++;
 	}
 
 	public void remove(int index) {
-		data[index] = 0;
+		// shift everything after index one space to the left
 		for (int i = index; i < data.length - 1; i++) {
 			data[i] = data[i + 1];
 		}
+		//change last index to 0
+		data[data.length-1] = 0;
+		//reduce number of elements
+		numberElements--;
 	}
 
 	public int get(int index) {
@@ -65,7 +73,7 @@ public class SuperArray {
 	}
 
 	public void grow() {
-		int[] temp = new int[data.length+1];
+		int[] temp = new int[data.length + 1];
 		for (int i = 0; i < data.length; i++) {
 			temp[i] = data[i];
 		}
