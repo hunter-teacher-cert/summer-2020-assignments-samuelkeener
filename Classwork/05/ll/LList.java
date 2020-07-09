35c20e7051637329eaf1aca7b858efb83650e0d0
Sam Keener <samuelbkeener@gmail.com>
2020-07-09 11:55:45 -0400

    added exception throws for day 07

--- Classwork/05/ll/LList.java
+++ Classwork/05/ll/LList.java
@@ -7,14 +7,26 @@ public class LList {
 
 	public LList() {
 		head = null;
+		length = 0;
 	}
 
 	// Add a new Node containing value to the front of the list
 	public void addFront(String value) {
+		if (value == null) {
+			throw new IllegalArgumentException();
+		}
 		Node add = new Node(value);
 		add.setNext(head);
 		head = add;
 		length++; // adjusts length of list
+	}
+
+	// Add a new Node to the end of the list
+	public void add(String value) {
+		if (value == null) {
+			throw new IllegalArgumentException();
+		}
+		insert(new Node(value), length);
 	}
 
 	// Returns true if list is empty, false otherwise
@@ -26,11 +38,11 @@ public class LList {
 	public String get(int index) {
 		Node tmp = head;
 		if (head == null) // prevents error if list is empty
-			return null;
+			throw new IndexOutOfBoundsException();
 		for (int i = 0; i < index; i++) {
 			tmp = tmp.getNext();
 			if (tmp == null) // returns null if index invalid
-				return null;
+				throw new IndexOutOfBoundsException();
 		}
 		return tmp.getData();
 	}
@@ -41,28 +53,23 @@ public class LList {
 		for (int i = 0; i < index; i++) {
 			tmp = tmp.getNext();
 			if (tmp == null) // does nothing if index is invalid
-				return;
+				throw new IndexOutOfBoundsException();
 		}
 		tmp.setData(value);
 	}
 
 	// Inserts Node with given value at index, pushes remaining forward
 	public void insert(int index, String value) {
-		if (index == 0)
-			addFront(value); // adds Node to front if index is 0
-		else {
-			Node tmp = head;
-			for (int i = 0; i < index - 1; i++) { // stops just before index so it can push the value at the given index
-													// forward
-				tmp = tmp.getNext();
-				if (tmp == null) // ends method if index invalid
-					return;
-			}
-			Node tmp2 = tmp.getNext(); // stores next Node in tmp variable
-			tmp.setNext(new Node(value, tmp2)); // creates new node after current index and sets its next value to tmp2
-		}
-		length++; // adjusts length of list
-	}
+		insert(new Node(value), index);
+		/*
+		 * if (index == 0) addFront(value); // adds Node to front if index is 0 else {
+		 * Node tmp = head; for (int i = 0; i < index - 1; i++) { // stops just before
+		 * index so it can push the value at the given index // forward tmp =
+		 * tmp.getNext(); if (tmp == null) // ends method if index invalid return; }
+		 * Node tmp2 = tmp.getNext(); // stores next Node in tmp variable
+		 * tmp.setNext(new Node(value, tmp2)); // creates new node after current index
+		 * and sets its next value to tmp2 } length++; // adjusts length of list
+		 */ }
 
 	// Inserts given Node at index, pushes remaining forward
 	public void insert(Node node, int index) {
@@ -74,7 +81,12 @@ public class LList {
 													// forward
 				tmp = tmp.getNext();
 				if (tmp == null) // ends method if index invalid
-					return;
+					throw new IndexOutOfBoundsException();
+			}
+			if (tmp.getNext() == null) {
+				tmp.setNext(node);
+				length++;
+				return;
 			}
 			node.setNext(tmp.getNext()); // attaches node to Node at target index
 			tmp.setNext(node); // attaches tmp to node
@@ -90,7 +102,8 @@ public class LList {
 				return i;
 			tmp = tmp.getNext();
 			if (tmp == null) // we've reached the end of the list and didn't find it, return -1
-				return -1;
+				throw new NoSuchElementException();
+
 		}
 	}
 
@@ -103,26 +116,24 @@ public class LList {
 			for (int i = 0; i < index - 1; i++) { // loops up until just before given index
 				tmp = tmp.getNext();
 				if (tmp == null)
-					return;
+					throw new NoSuchElementException();
 			}
-			Node tmp2 = tmp.getNext(); // creates reference to Node AFTER target index
-			tmp.setNext(tmp2.getNext()); // sets next of Node before target to the node AFTER target, thus removing
+
+			Node next = tmp.getNext(); // creates reference to Node AFTER target index
+			if (next == null)
+				throw new NoSuchElementException();
+			tmp.setNext(next.getNext()); // sets next of Node before target to the node AFTER target, thus removing
 		}
 		length--; // adjusts length of list
 	}
 
 	// returns length value
-	public int length() {
-		if (head == null)
-			return 0;
-		else {
-			Node tmp = head;
-			for (int i = 1;; i++) {
-				tmp = tmp.getNext();
-				if (tmp == null)
-					return i;
-			}
-		}
+	public int getLength() {
+		/*
+		 * if (head == null) return 0; else { Node tmp = head; for (int i = 1;; i++) {
+		 * tmp = tmp.getNext(); if (tmp == null) return i; } }
+		 */
+		return length;
 	}
 
 	public String toString() {
